e353639148f53deb8421bb7bd12a86408d5a778c
Ashton Mahatoo <amahatoosr@yahoo.com>
2024-09-29 23:19:03 -0400

    new

---
+++ Group5_SectionW03_Fall 2024_Part1_Report(ImplementationofClient-ServerParadigm)/Client.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+
+        /** The Socket class is used by both the client and server to read and write data.
+            A client uses the Socket class to initiate a connection to a server.*/
+        
+        Socket socket = new Socket("localhost", 4000);// Connect to Router on port 4000
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        /**Send message to Server via Router */
+        //String first_messageToSend = "client One,  Hello Server!";
+        String first_messageToSend = "From Client, Amdahl's law is an intuitive the observation that the performance improvement that can be gained through parallel processing is limited by the part of a system that's inherently sequential -- that is, the set of operations that must be run in series.";
+        //String first_messageToSend = " fnjnorvnqu3h5uino89983jr8438&&^534rfdfghvuecuheugey09(*&^ggtfytRCRE#$7779HHTFRFXRYtgv)";
+        
+        out.println(first_messageToSend);
+        System.out.println("Client sent: " + first_messageToSend);
+
+        long startTime = System.nanoTime();
+
+        // Code to be timed
+
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime); 
+
+        // Print the duration in nanoseconds
+        System.out.println("Duration: " + duration + " nanoseconds"); 
+
+        // Convert to milliseconds
+        long durationInMillis = duration / 1000000;
+        System.out.println("Duration: " + durationInMillis + " milliseconds"); 
+
+        
+        /**  Receive response from Server via Router**/
+        String response = in.readLine();
+        System.out.println("Client received: " + response + " via the Router");
+
+        // Close connection
+        socket.close();
+    }
+}
